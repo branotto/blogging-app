0e9e9579854b7f9bf75d4592da840db9195202aa
Brandon Otto <branaotto@gmail.com>
2018-04-25 00:31:08 -0400

    updated config for mLab database and Heroku

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABSEU_URL || 'mongodb://localhost/blogging-app';
+exports.DATABASE_URL = process.env.DATABSEU_URL || 'mongodb://DBadmin:passwordsarefun@ds157599.mlab.com:57599/otto-blogging-app';
 exports.PORT = process.env.PORT || 8080;
